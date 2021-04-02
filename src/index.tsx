c92402ae724eb894f96aa96701b4158e11c45ea8
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-04-02 10:16:06 +0430

    feat

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 /* main app file */
-import App from 'app';
+import Application from 'app';
 /* services */
 import reportWebVitals from 'reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
+import { ReactQueryService } from 'services/reactQuery/reactQueryService';
 
 // Initialize languages
 import 'locales/i18n';
@@ -15,9 +16,11 @@ const MOUNT_NODE = document.getElementById('root') as HTMLElement;
 ReactDOM.render(
     <React.StrictMode>
         <HelmetProvider>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <ReactQueryService>
+                <BrowserRouter>
+                    <Application />
+                </BrowserRouter>
+            </ReactQueryService>
         </HelmetProvider>
     </React.StrictMode>,
     MOUNT_NODE,
