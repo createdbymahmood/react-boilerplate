2af1ec62e2d7a3988f479ceb229bf5bf591010f6
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-04-02 10:09:09 +0430

    add generators

---
+++ src/app/pages/Pkg/Pkg.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+/* components */
+/* modules */
+import clsx from 'clsx';
+/* helpers */
+/* assets */
+/* services */
+/* constants */
+/* styles */
+import s from './Pkg.module.scss';
+/* types */
+
+const Pkg: FC = () => {
+    return <div className={s.pkg}>Pkg</div>;
+};
+
+export default Pkg;
