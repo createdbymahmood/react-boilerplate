be1bb567aa105036c3c5246b7fd0bad7afe47074
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-04-03 19:23:24 +0430

    edit craco config

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const CracoAlias = require('craco-alias');
+const WebpackBar = require('webpackbar');
 
 module.exports = {
     plugins: [
@@ -15,4 +16,35 @@ module.exports = {
             },
         },
     ],
+    webpack: {
+        alias: {},
+        plugins: {
+            add: [
+                new WebpackBar({ color: '#772194' }),
+            ] /* An array of plugins */,
+            remove: [] /* An array of plugin constructor's names (i.e. "StyleLintPlugin", "ESLintWebpackPlugin" ) */,
+        },
+        configure: {
+            /* Any webpack configuration options: https://webpack.js.org/configuration */
+        },
+        configure: (webpackConfig, { env, paths }) => {
+            return webpackConfig;
+        },
+    },
+    eslint: {
+        enable: false /* (default value) */,
+        mode: 'extends' /* (default value) */,
+        configure: {
+            /* Any eslint configuration options: https://eslint.org/docs/user-guide/configuring */
+        },
+        configure: (eslintConfig, { env, paths }) => {
+            return eslintConfig;
+        },
+        pluginOptions: {
+            /* Any eslint plugin configuration options: https://github.com/webpack-contrib/eslint-webpack-plugin#options. */
+        },
+        pluginOptions: (eslintOptions, { env, paths }) => {
+            return eslintOptions;
+        },
+    },
 };
