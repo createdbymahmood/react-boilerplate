b8b72255436195bd6823dd74f2506da97fbb8675
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-04-05 22:52:53 +0430

    refactor

--- src/app/hooks/api/user/useExample.ts
+++ src/app/hooks/api/user/useExample.ts
@@ -1,11 +1,7 @@
 /* constants */
 import API_URLS from 'constants/apiUrls';
 /* modules */
-import {
-    useMutation,
-    UseMutationOptions,
-    UseMutationResult,
-} from 'react-query';
+import { useMutation, UseMutationResult } from 'react-query';
 // import _fp from 'lodash/fp';
 /* services */
 import apiService, { AxiosError } from 'services/apiService';
@@ -24,18 +20,16 @@ async function fn(variables: TVariables): Promise<TData> {
     return (await apiService.post<TData>(API_URLS.login, variables)).data;
 }
 
-type TOptions = UseMutationOptions<TData, TError, TVariables, TContext>;
-type TDefinedOptions = 'onMutate' | 'onError' | 'mutationKey' | 'onSuccess';
-type TPrunedOptions = Omit<TOptions, TDefinedOptions>;
-
-export function useLogin(
-    options?: TPrunedOptions,
-): UseMutationResult<TData, TError, TVariables, TContext> {
+export function useLogin(): UseMutationResult<
+    TData,
+    TError,
+    TVariables,
+    TContext
+> {
     return useMutation(API_URLS.login, fn, {
         onMutate,
         onError,
         onSuccess,
-        ...options,
     });
 }
 
