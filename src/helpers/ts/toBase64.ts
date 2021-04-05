4e96d7db39b7a24829249f9de60ef8db78f9f0bb
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-04-06 00:21:37 +0430

    remove shits

--- src/helpers/ts/toBase64.ts
+++
@@ -1,7 +0,0 @@
-export const toBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
-    new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = error => reject(error);
-    });
