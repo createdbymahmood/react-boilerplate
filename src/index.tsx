10ad6bf0f11bd1dd25d8d8e1079f7eb2cd0505fd
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-04-07 18:48:04 +0430

    import tw styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 /* main app file */
 import Application from 'app';
+
 /* services */
 import reportWebVitals from 'reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { ReactQueryService } from 'services/reactQuery';
 
-// Initialize languages
+/* Initialize languages */
 import 'services/i18n/i18n';
+
+/* styles */
+import 'assets/styles/tw.css';
 
 const MOUNT_NODE = document.getElementById('root') as HTMLElement;
 
