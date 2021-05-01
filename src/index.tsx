553421adce509e8c7e360593b8f5935ac527fe09
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-05-02 00:30:29 +0430

    feat: setup PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/* PWA setup */
+import * as serviceWorkerRegistration from 'serviceWorkerRegistration';
+
 /* main app file */
 import Application from 'app';
 
@@ -31,4 +34,12 @@ ReactDOM.render(
     MOUNT_NODE,
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
