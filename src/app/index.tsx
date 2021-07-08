77c2ebad4ea76f17c34a348f710801dd8d17767f
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-07-08 10:35:46 +0430

    change path of routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import RouteFactory from 'services/routerService/RouteFactory';
-import Routes from '@app/routes/routes';
+import Routes from 'services/routerService/routes';
 
 export default function App() {
     return <RouteFactory routes={Routes} />;
