3057d0eef2ca4458888def9fcd984684481d8ade
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-07-30 18:52:23 +0430

    remove extra children destructuring

--- src/services/router/RouterService.tsx
+++ src/services/router/RouterService.tsx
@@ -1,9 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { BrowserRouter, BrowserRouterProps } from 'react-router-dom';
 
-export function RouterService({
-    children,
-    ...props
-}: PropsWithChildren<BrowserRouterProps>) {
-    return <BrowserRouter {...props}>{children}</BrowserRouter>;
+export function RouterService(props: PropsWithChildren<BrowserRouterProps>) {
+    return <BrowserRouter {...props} />;
 }
