394868ec315421f809deb4bb0de52d3eab5f00f8
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-07-30 23:03:47 +0430

    fix

--- src/app/pages/Index.tsx
+++ src/app/pages/Index.tsx
@@ -2,7 +2,7 @@ import { Box, Link } from '@components';
 
 export default function Index() {
     return (
-        <Box as={Link} to='Profile' params={{ userId: '123', laptopId: '123' }}>
+        <Box as={Link} to='Index'>
             Index Page
         </Box>
     );
