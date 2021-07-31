30186e2e5c20a1ad8651c27c98ed11faf31d2c94
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-07-31 12:26:36 +0430

    refactor

---
+++ src/app/hooks/overlay/useOverlayConfig.ts
@@ -0,0 +1,29 @@
+import { AppRoutesPath } from '@components';
+import { createRoute } from 'helpers/ts/createRoute';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useEffectOnce } from 'react-use';
+
+type Props = {
+    onCloseRedirectTo: AppRoutesPath;
+    timeout: number;
+};
+
+export function useOverlayConfig({ onCloseRedirectTo, timeout }: Props) {
+    const history = useHistory();
+    const [open, setOpen] = useState(false);
+
+    /* Open modal on first mount */
+    useEffectOnce(() => {
+        setOpen(true);
+    });
+
+    const handleClose = () => {
+        setOpen(false);
+        setTimeout(() => {
+            history.push(createRoute(onCloseRedirectTo));
+        }, timeout);
+    };
+
+    return [open, handleClose];
+}
