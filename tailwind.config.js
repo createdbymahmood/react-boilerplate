d2154bf5cea9cef684b81ac8e2bebb358f861dd1
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-08-05 23:57:25 +0430

    add @tailwindcss/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                rose: colors.rose,
+            },
+        },
     },
     variants: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require('@tailwindcss/forms')],
 };
