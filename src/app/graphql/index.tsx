1f3448a48700a00342f262d84db766a606c45a69
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-09-11 16:54:43 +0430

    do some refactors

--- src/app/graphql/index.tsx
+++ src/app/graphql/index.tsx
@@ -4,10 +4,12 @@ export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = {
     [K in keyof T]: T[K];
 };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-    { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-    { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+    [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+    [SubKey in K]: Maybe<T[SubKey]>;
+};
 const defaultOptions = {};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -21,15 +23,15 @@ export type Scalars = {
 };
 
 export enum CacheControlScope {
-    Public = 'PUBLIC',
     Private = 'PRIVATE',
+    Public = 'PUBLIC',
 }
 
 export type Continent = {
     __typename?: 'Continent';
     code: Scalars['ID'];
-    name: Scalars['String'];
     countries: Array<Country>;
+    name: Scalars['String'];
 };
 
 export type ContinentFilterInput = {
@@ -38,23 +40,23 @@ export type ContinentFilterInput = {
 
 export type Country = {
     __typename?: 'Country';
+    capital: Maybe<Scalars['String']>;
     code: Scalars['ID'];
+    continent: Continent;
+    currency: Maybe<Scalars['String']>;
+    emoji: Scalars['String'];
+    emojiU: Scalars['String'];
+    languages: Array<Language>;
     name: Scalars['String'];
     native: Scalars['String'];
     phone: Scalars['String'];
-    continent: Continent;
-    capital: Maybe<Scalars['String']>;
-    currency: Maybe<Scalars['String']>;
-    languages: Array<Language>;
-    emoji: Scalars['String'];
-    emojiU: Scalars['String'];
     states: Array<State>;
 };
 
 export type CountryFilterInput = {
     code: Maybe<StringQueryOperatorInput>;
-    currency: Maybe<StringQueryOperatorInput>;
     continent: Maybe<StringQueryOperatorInput>;
+    currency: Maybe<StringQueryOperatorInput>;
 };
 
 export type Language = {
@@ -71,20 +73,20 @@ export type LanguageFilterInput = {
 
 export type Query = {
     __typename?: 'Query';
-    continents: Array<Continent>;
     continent: Maybe<Continent>;
+    continents: Array<Continent>;
     countries: Array<Country>;
     country: Maybe<Country>;
-    languages: Array<Language>;
     language: Maybe<Language>;
-};
-
-export type QueryContinentsArgs = {
-    filter: Maybe<ContinentFilterInput>;
+    languages: Array<Language>;
 };
 
 export type QueryContinentArgs = {
     code: Scalars['ID'];
+};
+
+export type QueryContinentsArgs = {
+    filter: Maybe<ContinentFilterInput>;
 };
 
 export type QueryCountriesArgs = {
@@ -95,28 +97,28 @@ export type QueryCountryArgs = {
     code: Scalars['ID'];
 };
 
-export type QueryLanguagesArgs = {
-    filter: Maybe<LanguageFilterInput>;
-};
-
 export type QueryLanguageArgs = {
     code: Scalars['ID'];
+};
+
+export type QueryLanguagesArgs = {
+    filter: Maybe<LanguageFilterInput>;
 };
 
 export type State = {
     __typename?: 'State';
     code: Maybe<Scalars['String']>;
-    name: Scalars['String'];
     country: Country;
+    name: Scalars['String'];
 };
 
 export type StringQueryOperatorInput = {
     eq: Maybe<Scalars['String']>;
-    ne: Maybe<Scalars['String']>;
+    glob: Maybe<Scalars['String']>;
     in: Maybe<Array<Maybe<Scalars['String']>>>;
+    ne: Maybe<Scalars['String']>;
     nin: Maybe<Array<Maybe<Scalars['String']>>>;
     regex: Maybe<Scalars['String']>;
-    glob: Maybe<Scalars['String']>;
 };
 
 export type _Country_Fragment_ = {
--- src/app/pages/Index.tsx
+++ src/app/pages/Index.tsx
@@ -1,10 +1,21 @@
 import { FC } from 'react';
 import { Box, DocumentTitle } from '@components';
+import { useCountries } from '@graphql';
+import { snakeCase } from 'lodash';
+
+function useCountriesElement() {
+    const { loading, data } = useCountries();
+    if (loading) return `loading...`;
+    return data?.countries.map(c => <div>{snakeCase(c.name)}</div>);
+}
 
 const Index: FC = () => {
+    const ce = useCountriesElement();
+
     return (
         <Box>
             <DocumentTitle title='Home' />
+            {ce}
         </Box>
     );
 };
