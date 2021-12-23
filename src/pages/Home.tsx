f984bd83b843b495fc3dd50f90354af618d96778
Mahmood Bagheri <createdbymahmood@gmail.com>
2021-12-23 15:05:57 +0330

    fix test s

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,5 @@
-import {
-    CircularProgress,
-    circularProgressClasses,
-    Box,
-    Button,
-} from '@mui/material';
 import React from 'react';
-function FacebookCircularProgress(props) {
-    return (
-        <Box sx={{ position: 'relative' }}>
-            <CircularProgress
-                variant='determinate'
-                sx={{
-                    color: theme =>
-                        theme.palette.grey[
-                            theme.palette.mode === 'light' ? 200 : 800
-                        ],
-                }}
-                size={40}
-                thickness={4}
-                {...props}
-                value={100}
-            />
-            <CircularProgress
-                variant='indeterminate'
-                disableShrink
-                sx={{
-                    color: theme =>
-                        theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
-                    animationDuration: '550ms',
-                    position: 'absolute',
-                    left: 0,
-                    [`& .${circularProgressClasses.circle}`]: {
-                        strokeLinecap: 'round',
-                    },
-                }}
-                size={40}
-                thickness={4}
-                {...props}
-            />
-        </Box>
-    );
-}
+
 export default function Home() {
-    return (
-        <div>
-            <FacebookCircularProgress></FacebookCircularProgress>
-            <Button>Salam</Button>
-        </div>
-    );
+    return <div></div>;
 }
--- src/services/theme/shape.ts
+++ src/services/theme/shape.ts
@@ -1,16 +1,9 @@
 // ----------------------------------------------------------------------
 
-declare module '@mui/system' {
-  interface Shape {
-    borderRadiusSm: number | string;
-    borderRadiusMd: number | string;
-  }
-}
-
 const shape = {
-  borderRadius: 8,
-  borderRadiusSm: 12,
-  borderRadiusMd: 16
+    borderRadius: 8,
+    borderRadiusSm: 12,
+    borderRadiusMd: 16,
 };
 
 export default shape;
