865988b8d346fadf732554972bf6027879fa9415
Mahmood Bagheri <createdbymahmood@gmail.com>
2022-02-11 14:40:17 +0330

    fix the link part

--- src/helpers/ts/createRoute.ts
+++ src/helpers/ts/createRoute.ts
@@ -1,10 +1,11 @@
 import generatePath, { ParamMap } from 'urlcat';
 import { Path } from 'types/global';
 import { paths } from 'constants/paths';
+import { get } from 'lodash';
 
 export const createRoute = (
     path: Path<typeof paths>,
     params: ParamMap = {},
 ) => {
-    return generatePath(path, params);
+    return generatePath(get(paths, path), params);
 };
